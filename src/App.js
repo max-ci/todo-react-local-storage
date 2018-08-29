cca5775b1fc8c69477659e7b6a449519b310def9
Max <>
2018-08-29 21:04:06 +0200

    Moved state from Sidebar to main App component

--- src/App.js
+++ src/App.js
@@ -1,44 +1,98 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Header from './components/Header';
 import Sidebar from './components/Sidebar';
+import Categories from './components/Categories';
+//import Tasks from './components/Tasks';
+//import Modal from './components/Modal';
 import InputError from './components/InputError';
 
 class App extends Component {
-
   constructor(props) {
     super(props);
 
     let categories = JSON.parse(localStorage.getItem('categories'));
-    if(categories === null) {
+    if (categories === null) {
       categories = [];
     }
     let tasks = JSON.parse(localStorage.getItem('categories'));
-    if(tasks === null) {
+    if (tasks === null) {
       tasks = [];
     }
 
     this.state = {
-      czesc: 'czesfsdfsdafsdafc',
+      mobileMenu: '',
       categories: categories,
       tasks: tasks,
       error: {
         active: false,
         content: ''
       }
-    }
+    };
 
+    this.toggleMobileMenu = this.toggleMobileMenu.bind(this);
+    this.handleAddCategory = this.handleAddCategory.bind(this);
+    this.handleEditCategory = this.handleEditCategory.bind(this);
+    this.handleDeleteCategory = this.handleDeleteCategory.bind(this);
     this.handleShowError = this.handleShowError.bind(this);
     this.handleCloseError = this.handleCloseError.bind(this);
   }
 
+  toggleMobileMenu() {
+    if (this.state.mobileMenu) {
+      this.setState({
+        mobileMenu: ''
+      });
+    } else {
+      this.setState({
+        mobileMenu: 'active'
+      });
+    }
+  }
+
+  saveToLocalStorage(categories) {
+    localStorage.setItem('categories', JSON.stringify(categories));
+  }
+
+  handleAddCategory(name) {
+    let categories = this.state.categories;
+    categories.push({
+      id: Math.floor(Math.random() * 10000 + 1),
+      title: name
+    });
+    this.setState({
+      categories: categories
+    });
+    this.saveToLocalStorage(categories);
+  }
+
+  handleDeleteCategory(id) {
+    let categories = this.state.categories;
+    let index = categories.findIndex(x => x.id === id);
+    categories.splice(index, 1);
+    this.setState({
+      categories: categories
+    });
+    this.saveToLocalStorage(categories);
+  }
+
+  handleEditCategory(id, name) {
+    let categories = this.state.categories;
+    let index = categories.findIndex(x => x.id === id);
+    categories[index].title = name;
+    this.setState({
+      categories: categories
+    });
+    this.saveToLocalStorage(categories);
+  }
+
   handleShowError(content) {
-    if(!this.state.error.active) {
+    if (!this.state.error.active) {
       setTimeout(() => {
         this.handleCloseError();
       }, 3000);
     }
-
     this.setState({
       error: {
         active: true,
@@ -57,14 +111,31 @@ class App extends Component {
   }
 
   render() {
-    return(
+    return (
       <div>
-        <Sidebar categories={this.state.categories} showError={this.handleShowError} />
-        <InputError error={this.state.error} closeError={this.handleCloseError} />
+        <Header onClick={this.toggleMobileMenu} />
+        <Sidebar
+          categories={this.state.categories}
+          showError={this.handleShowError}
+          mobileMenu={this.state.mobileMenu}
+        >
+          <Categories
+            categories={this.state.categories}
+            onAdd={this.handleAddCategory}
+            onEdit={this.handleEditCategory}
+            onDelete={this.handleDeleteCategory}
+            showError={this.handleShowError}
+          />
+        </Sidebar>
+        {/* <Tasks /> */}
+        {/* <Modal /> */}
+        <InputError
+          error={this.state.error}
+          closeError={this.handleCloseError}
+        />
       </div>
     );
   }
-
 }
 
 export default App;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,77 +1,16 @@
 import React, { Component } from 'react';
 import './Sidebar.css';
-import Categories from './Categories';
 import logo from './../images/logo.png';
 
 class Sidebar extends Component {
 
-  constructor(props) {
-    super(props);
-
-    let categories = JSON.parse(localStorage.getItem('categories'));
-    if(categories === null) {
-      categories = [];
-    }
-    let tasks = JSON.parse(localStorage.getItem('categories'));
-    if(tasks === null) {
-      tasks = [];
-    }
-    this.state = {
-      active: true,
-      categories: categories,
-      activeCategory: null,
-      tasks: tasks
-    }
-
-    this.handleAddCategory = this.handleAddCategory.bind(this);
-    this.handleDeleteCategory = this.handleDeleteCategory.bind(this);
-    this.handleEditCategory = this.handleEditCategory.bind(this);
-    this.showError = this.showError.bind(this);
-  }
-
-  handleAddCategory(name) {
-    let categories = this.state.categories;
-    categories.push({
-      id: Math.floor((Math.random() * 10000) + 1),
-      title: name
-    });
-    this.setState({
-      categories: categories
-    });
-    localStorage.setItem('categories', JSON.stringify(categories));
-  }
-
-  handleDeleteCategory(id) {
-    let categories = this.state.categories;
-    let index = categories.findIndex(x => x.id === id);
-    categories.splice(index, 1);
-    this.setState({
-      categories: categories
-    });
-    localStorage.setItem('categories', JSON.stringify(categories));
-  }
-
-  handleEditCategory(id, name) {
-    let categories = this.state.categories;
-    let index = categories.findIndex(x => x.id === id);
-    categories[index].title = name;
-    this.setState({
-      categories: categories
-    });
-    localStorage.setItem('categories', JSON.stringify(categories));
-  }
-
-  showError(content) {
-    this.props.showError(content);
-  }
-
   render() {
     return (
-      <div className="sidebar">
+      <div className={'sidebar ' + this.props.mobileMenu}>
         <div className="sidebar-logo-wrapper">
           <img src={logo} className="sidebar-logo" alt="Todo App" />
         </div>
-        <Categories categories={this.state.categories} onDelete={this.handleDeleteCategory} onAdd={this.handleAddCategory} onEdit={this.handleEditCategory} showError={this.showError} />
+        {this.props.children}
       </div>
     );
   }
