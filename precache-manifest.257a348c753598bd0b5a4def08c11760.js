19f3bde115235d575ad85a2e781381e1c8e91997
Max <m4ks@o2.pl>
2019-02-05 15:33:47 +0100

    Updates

---
+++ precache-manifest.257a348c753598bd0b5a4def08c11760.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5fa7007a33db8ed9675d",
+    "url": "/todo-react-local-storage/static/css/main.4da4d96b.chunk.css"
+  },
+  {
+    "revision": "5fa7007a33db8ed9675d",
+    "url": "/todo-react-local-storage/static/js/main.5fa7007a.chunk.js"
+  },
+  {
+    "revision": "61ea47c5b11cf55d2c64",
+    "url": "/todo-react-local-storage/static/css/1.693d96b6.chunk.css"
+  },
+  {
+    "revision": "61ea47c5b11cf55d2c64",
+    "url": "/todo-react-local-storage/static/js/1.61ea47c5.chunk.js"
+  },
+  {
+    "revision": "b65e7fd507a1e33164fb",
+    "url": "/todo-react-local-storage/static/js/runtime~main.b65e7fd5.js"
+  },
+  {
+    "revision": "15dced94158041d02b3ebf3805e841a5",
+    "url": "/todo-react-local-storage/index.html"
+  }
+];
